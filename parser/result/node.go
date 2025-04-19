32b784168b89d1ce3c1f75c6da7c6a20f7d656f7
SSripilaipong <SHSnail@mail.com>
2025-04-19 16:39:14 +0700

    refactor(parser.result): remove ParseNode() since not being used

--- parser/result/node.go
+++ parser/result/node.go
@@ -1,25 +1,12 @@
 package result
 
 import (
-	"github.com/SSripilaipong/muto/common/fn"
 	ps "github.com/SSripilaipong/muto/common/parsing"
 	"github.com/SSripilaipong/muto/common/rslt"
 	"github.com/SSripilaipong/muto/common/tuple"
 	psBase "github.com/SSripilaipong/muto/parser/base"
 	stResult "github.com/SSripilaipong/muto/syntaxtree/result"
 )
-
-var ParseNode = fn.Compose3(psBase.FilterStatement, RsNode, psBase.StringToCharTokens)
-
-var RsNode = ps.Map(rslt.Value, Node())
-
-func Node() func([]psBase.Character) []tuple.Of2[stResult.Node, []psBase.Character] {
-	return ps.Or(
-		nonNestedNode,
-		ps.Map(castObjectNode, psBase.InParentheses(NakedObjectMultilines)),
-		ps.Map(stResult.ToNode, structure),
-	)
-}
 
 var RsSimplifiedNode = ps.Map(rslt.Value, SimplifiedNode())
 
